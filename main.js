ad883a4f78cdeac0dce552006507a07609e670bf
vijayskk <vijayskk007@gmail.com>
2022-06-28 10:49:26 +0530

    resize code removed

--- main.js
+++ main.js
@@ -157,13 +157,3 @@ function animate(){
 animate()
 
 
-window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize(){
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-}
