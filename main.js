e2409cffc5d5e3b56a036e47933ccdb9f033bd00
vijayskk <vijayskk007@gmail.com>
2022-06-28 10:35:30 +0530

    Basic experience

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 import { AdaptiveToneMappingPass } from 'three/examples/jsm/postprocessing/AdaptiveToneMappingPass';
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass';
+import { AfterimagePass } from 'three/examples/jsm/postprocessing/AfterimagePass';
 
 
 const canvas = document.querySelector('#bg')
-
+const audio = document.getElementById('audio')
+audio.volume = 0.2
+audio.play()
 const { width , height } = canvas.getBoundingClientRect()
 
 const scene = new THREE.Scene()
@@ -52,9 +56,9 @@ pl2.position.set(0,1.8,-5)
 scene.add(pl2)
 
 
-const pl3 = new THREE.PointLight(0xfff2d9,0.3)
+const pl3 = new THREE.PointLight(0xfff2d9,2)
 pl3.target = box
-pl3.position.set(0,1.8,-12)
+pl3.position.set(2,.9,-11)
 //scene.add(pl3)
 
 
@@ -113,6 +117,18 @@ composer.addPass( imagePass );
 const image2Pass = new AdaptiveToneMappingPass();
 composer.addPass( image2Pass );
 
+const image4Pass = new AfterimagePass(0.001);
+composer.addPass( image4Pass );
+
+const image3Pass = new GlitchPass(0);
+composer.addPass( image3Pass );
+
+
+
+const color = 0xff0000;
+const density = 0.01;
+scene.fog = new THREE.FogExp2(color, density);
+
 function animate(){
   
   //controlls.update()
@@ -121,7 +137,7 @@ function animate(){
     camera.position.z += speed;
   
   }
-  if(isBackward  && camera.position.z > -9){
+  if(isBackward  && camera.position.z > -7.5){
     camera.position.z-= speed;
     }
   // if(isRight){
